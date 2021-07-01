5e4be35b82f4284e9a8225746f8c631d8597b3c1
Athi Sithembiso <asithembiso@shoprite.co.za>
2021-07-01 16:29:19 +0200

    added mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
 import express from 'express'
-import { config } from 'dotenv'
-import products from './data/products.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
-config()
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
     res.send('API is running!!')
 }) 
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
-
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
